c823a7632694b60ed8a05ad1ee85f69d2cd8434a
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2022-12-13 13:18:44 -0800

    Made all the c files needed for the project

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,47 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 - print alphabet 10 times
- */
-void print_alphabet_x10(void)
-{
-	int ten;
-	char la;
 
-	for (ten = 0; ten <= 9; ten++)
-	(
-	        for (la = 'a' ; la <= 'z'; la++)
-		        _putchar(la);
-		_putcahr('\n')
-	)
+
+/**
+ *
+ *  * print_alphabet_x10 - Make alphabet x10 times
+ *
+ *   *
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_alphabet_x10(void)
+
+{
+
+		char c;
+
+			int i = 0;
+
+
+
+				while (i <= 9)
+
+						{
+
+									for (c = 'a'; c <= 'z'; c++)
+
+												{
+
+																_putchar(c);
+
+																		}
+
+
+
+											_putchar('\n');
+
+
+
+													i++;
+
+														}
+
 }
