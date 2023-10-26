0eedff6bd5066ca58073b760d7092a3605457435
Abdulsamad Adeosun <adeosunabdulsamad25@gmail.com>
2023-10-26 15:42:01 +0100

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,3 +5,14 @@
  * @s: string to print
  */
 
+int _strlen_recursion(char *s)
+{
+    if (*s)
+    {
+        return(1 + _strlen_recursion(s + 1));
+    }
+    else
+    {
+        return(0);
+    }
+}
