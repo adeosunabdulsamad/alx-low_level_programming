839b8cee752868b43ac0088a5e7cda14470b6bc0
Abdulsamad Adeosun <adeosunabdulsamad25@gmail.com>
2023-10-26 16:06:01 +0100

    rec

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * pow_recursion - returns the factorial of a number
+ * @x: number to return the factorial from
+ * @y: number to return the factorial from
+ *
+ * Return: factorial of n
+ */
