457184cc48038b52bc2add59a964dd6c9379c7c8
Abdulsamad Adeosun <adeosunabdulsamad25@gmail.com>
2023-10-26 07:40:03 +0100

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - prints a string, followed by a new line
+ * @s: string to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+    if (*s)
+    {
+        _print_rev_recursion(s + 1);
+        _putchar(*s);
+    }
+}
