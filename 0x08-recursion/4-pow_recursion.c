1b593fe4b9a4319df246314ca452894e456e4ce3
Abdulsamad Adeosun <adeosunabdulsamad25@gmail.com>
2023-10-26 19:51:16 +0100

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,22 @@
 #include "main.h"
 
 /**
- * pow_recursion - returns the factorial of a number
- * @x: number to return the factorial from
- * @y: number to return the factorial from
+ * pow_recursion - returns number x raised to a power of numy
+ * @x: number that will be raised
+ * @y: number that raises num x
  *
- * Return: factorial of n
+ * Return: pow_recursion of x,y
  */
+
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    {
+        return(-1);
+    }
+    if (y == 0)
+    {
+        return(1);
+    }
+    return(x * _pow_recursion(x, y - 1));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * sqrt_recursion - returns square root of a number
+ * @n: number that will have a square root
+ *
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+    if 
+}
