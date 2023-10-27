d6936dfae228891947a6520cf8c997972b8a4e4a
adeosun abdulsamad <adeosunabdulsamad25@gmail.com>
2023-10-27 14:22:55 -0700

    checking argv werror error out

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @argc - count of arguments
+ * main - function to be run that returns integer
+ * @argv - vector of argument
+ *
+ * return - returns value of zero
+ */
+
+ int main (int argc, char *argv[])
+{
+	printf("%d\n", argc-1);
+}
