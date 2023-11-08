761a15f5e0ec4fc47a9080061bd018d1ebb8a690
adeosun abdulsamad <adeosunabdulsamad25@gmail.com>
2023-11-08 13:17:08 -0800

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <stddef.h>
 
 int _putchar(char a);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
