678aadfd0043fea4ed50b46665603013ee11c54f
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2023-11-27 07:50:17 -0800

    mixed declaration error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-
+#include <stdlib.h>
 /*
  *
  *
@@ -8,15 +8,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	char *buffer;
 	if (filename==NULL)
 		return 0;
-	FILE *fd;
+	buffer = malloc(sizeof(char) * letters);
 	fd = open(filename, O_RDWR | O_CREAT | S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return 0;
-	char buffer[letters];
-	ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
-        _putchar(fd, buffer, sizeof(buffer) - 1);
+	ssize_t bytesRead = read(fd, buffer, letters);
 	close(fd);
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		return 0;
+	}
+	free(buffer);
+	dprintf(STDOUT_FILENO, "%.*s", (int)bytesRead, buffer);
 	return bytesRead;
 }
