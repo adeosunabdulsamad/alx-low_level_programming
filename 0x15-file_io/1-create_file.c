0c7a2ffea27f87f9cc4a38c58af39d8a42786b20
Abdulsamad Adeosun <59876143+adeosunabdulsamad@users.noreply.github.com>
2023-11-27 17:56:39 +0100

    Update 1-create_file.c
    Added the truncate function and updated my comments

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,12 @@
 #include "main.h"
 
 /**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- *
- *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -14,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	char *buffer;
 	if (filename == NULL)
 		return -1;
-        fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+        fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | O_TRUNC);
 	if (fd == -1)
 		return -1;
 	buffer = text_content;
