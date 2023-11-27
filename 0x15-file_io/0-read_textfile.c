d8b67b6192e5c7812dd6bd98c63d841e50ce6ee1
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2023-11-27 07:03:11 -0800

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,4 +10,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	if (filename==NULL)
 		return 0;
+	FILE *fd;
+	fd = open(filename, O_RDWR | O_CREAT | S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return 0;
+	char buffer[letters];
+	ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
+        _putchar(fd, buffer, sizeof(buffer) - 1);
+	close(fd);
+	return bytesRead;
 }
